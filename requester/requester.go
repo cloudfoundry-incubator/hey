c973b3f15dfbce85e3e69d081e70f19f6cc615d6
Valentin Kuznetsov <vkuznet@gmail.com>
2017-01-13 14:28:01 -0500

    Move Certs into main and made them part of requestor

--- requester/requester.go
+++ requester/requester.go
@@ -29,38 +29,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/vkuznet/x509proxy"
 	"golang.org/x/net/http2"
 )
-
-// Return client X509 certificates
-func Certs() (tls_certs []tls.Certificate) {
-	uproxy := os.Getenv("X509_USER_PROXY")
-	uckey := os.Getenv("X509_USER_KEY")
-	ucert := os.Getenv("X509_USER_CERT")
-	if len(uproxy) > 0 {
-		// use local implementation of LoadX409KeyPair instead of tls one
-		x509cert, err := x509proxy.LoadX509Proxy(uproxy)
-		if err != nil {
-			fmt.Println("Fail to parser proxy X509 certificate", err)
-			return
-		}
-		tls_certs = []tls.Certificate{x509cert}
-	} else if len(uckey) > 0 {
-		x509cert, err := tls.LoadX509KeyPair(ucert, uckey)
-		if err != nil {
-			fmt.Println("Fail to parser user X509 certificate", err)
-			return
-		}
-		tls_certs = []tls.Certificate{x509cert}
-	} else {
-		return
-	}
-	return
-}
-
-// load X509 certificates once
-var certs = Certs()
 
 const heyUA = "hey/0.0.1"
 
@@ -114,6 +84,9 @@ type Work struct {
 	// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
 	// Optional.
 	ProxyAddr *url.URL
+
+	// X509 certificates
+	Certs []tls.Certificate
 
 	results chan *result
 }
@@ -212,7 +185,7 @@ func (b *Work) runWorker(n int) {
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{
-			Certificates:       certs,
+			Certificates:       b.Certs,
 			InsecureSkipVerify: true,
 		},
 		DisableCompression: b.DisableCompression,
